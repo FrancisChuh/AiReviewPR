9c036f1fc0b03ea03bc7993147cceaec08301ded
Francis·ChuHao <31614448+FrancisChuh@users.noreply.github.com>
2025-12-13 21:11:41 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -120,6 +120,7 @@ async function getHeadDiffContext() {
 }
 
 async function aiCheckDiffContext() {
+  console.log(useChinese ? "run in aiCheckDiffContext：检查开始" : "run in aiCheckDiffContext: Start ai check")
   try {
     let commit_sha_url = `${process.env.GITHUB_SERVER_URL}/${process.env.INPUT_REPOSITORY}/src/commit/${process.env.GITHUB_SHA}`;
     let items: Array<any> = review_pull_request ? await getPrDiffContext() : await getHeadDiffContext();
